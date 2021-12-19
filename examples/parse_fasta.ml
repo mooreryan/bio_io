b48cfb92bde8085b5ca960b501cad6a6167c7454
Ryan M. Moore <rmm1047@gmail.com>
2021-12-18 23:17:51 -0500

    Add tests for reads from fifo

---
+++ examples/parse_fasta.ml
@@ -0,0 +1,16 @@
+open! Base
+open! Bio_io.Fasta
+
+let parse_argv () =
+  match Sys.get_argv () with
+  | [| _; file_name |] -> file_name
+  | _ -> failwith "missing file_name"
+
+let () =
+  let file_name = parse_argv () in
+  let num_seqs, total_bases =
+    In_channel.with_file_fold_records_exn file_name ~init:(0, 0)
+      ~f:(fun (num_seqs, total_bases) r ->
+        (num_seqs + 1, total_bases + Record.seq_length r))
+  in
+  Stdio.printf "num seqs: %d, num bases: %d\n" num_seqs total_bases
