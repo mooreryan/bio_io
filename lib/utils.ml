890d84a2cc4fa935717cc29ee3448988f6e5acb6
Ryan M. Moore <rmm1047@gmail.com>
2023-07-19 11:58:41 -0400

    Remove unused functions

--- lib/utils.ml
+++ lib/utils.ml
@@ -23,23 +23,6 @@ let try1' ~msg f a =
   | result ->
       Or_error.return result
 
-(* Run a fold-like function catching errors. *)
-let try_fold f_ a ~init ~f =
-  match f_ a ~init ~f with
-  | exception exn ->
-      Or_error.error "Caught exception" exn Exn.sexp_of_t
-  | result ->
-      Or_error.return result
-
-(* Run a map-like function catching errors. Also good for iter-like
-   functions. *)
-let try_map f_ a ~f =
-  match f_ a ~f with
-  | exception exn ->
-      Or_error.error "Caught exception" exn Exn.sexp_of_t
-  | result ->
-      Or_error.return result
-
 (* Complement dna:
    https://arep.med.harvard.edu/labgc/adnan/projects/Utilities/revcomp.html.contents
 
